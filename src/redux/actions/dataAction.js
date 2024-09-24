75cc50e161d0b4bc85082464a51b732c47db5628
Nikodemus HS <101912576+nikodemushs@users.noreply.github.com>
2024-09-24 14:52:30 +0700

    remove api

--- src/redux/actions/dataAction.js
+++ src/redux/actions/dataAction.js
@@ -11,7 +11,7 @@ import { data } from "../../Data";
 export const getAllHeroes = () => async (dispatch, getState) => {
   try {
     const response =
-      await axios.get(`https://api.opendota.com/api/heroStats?api_key=b4888f99-95b4-4b8e-80bb-b1871752b696
+      await axios.get(`https://api.opendota.com/api/heroStats
     `);
     console.log("REDUX response", response.data);
     dispatch(setHeroes(response.data));
@@ -77,7 +77,7 @@ export const getMatchups = () => async (dispatch, getState) => {
     const id = getState().data.heroId;
     console.log("id MATCHUP :>> ", id);
     const response = await axios.get(
-      `https://api.opendota.com/api/heroes/${id}/matchups?api_key=b4888f99-95b4-4b8e-80bb-b1871752b696
+      `https://api.opendota.com/api/heroes/${id}/matchups
       `
     );
     console.log("Matchup Response", response.data);
@@ -94,7 +94,7 @@ export const getMatchups = () => async (dispatch, getState) => {
 export const getMatchupDetails = () => async (dispatch, getState) => {
   try {
     const response =
-      await axios.get(`https://api.opendota.com/api/heroStats?api_key=b4888f99-95b4-4b8e-80bb-b1871752b696
+      await axios.get(`https://api.opendota.com/api/heroStats
     `);
     const heroesMap = {};
     response.data.forEach((hero) => {
